cd0ca9a4565391549d04609642cde335a42627de
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-16 09:53:46 +0700

    cardview berhasiel

--- AlfianRochmatulIrman_E41181407/MyApplication4/app/src/main/java/com/example/myapplication/MahasiswaAdapter.java
+++ AlfianRochmatulIrman_E41181407/MyApplication4/app/src/main/java/com/example/myapplication/MahasiswaAdapter.java
@@ -1,5 +1,51 @@
 package com.example.myapplication;
 
-public class MahasiswaAdapter {
-    
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.MahasiswaViewHolder> {
+
+    private ArrayList<mahasiswa> dataList;
+
+    public MahasiswaAdapter(ArrayList<mahasiswa> dataList){
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public MahasiswaAdapter.MahasiswaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        return new MahasiswaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MahasiswaAdapter.MahasiswaViewHolder holder, int position) {
+        holder.txtNama.setText(dataList.get(position).getNama());
+        holder.txtNpm.setText(dataList.get(position).getNpm());
+        holder.txtNoHp.setText(dataList.get(position).getNohp());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return (dataList != null) ? dataList.size() : 0;
+    }
+
+    public class MahasiswaViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtNama, txtNpm, txtNoHp;
+        public MahasiswaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNama = (TextView) itemView.findViewById(R.id.txt_nama_mahasiswa);
+            txtNpm = (TextView) itemView.findViewById(R.id.txt_npm_mahasiswa);
+            txtNoHp = (TextView) itemView.findViewById(R.id.txt_nohp_mahasiswa);
+        }
+    }
 }
--- AlfianRochmatulIrman_E41181407/MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AlfianRochmatulIrman_E41181407/MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<mahasiswa> mahasiswaArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addData();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        MahasiswaAdapter adapter = new MahasiswaAdapter(mahasiswaArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new mahasiswa("Alfian Rochmatul Irman", "E41181407", "081252223123"));
+        mahasiswaArrayList.add(new mahasiswa("Romat", "E41283213", "10231230"));
+        mahasiswaArrayList.add(new mahasiswa("asd", "1321312", "123912"));
     }
 }
