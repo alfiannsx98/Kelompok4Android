70c70d9ab0d86d1bfd2f3e4fedc275ebde7a8e17
Muhammad Arif Annaili Fitrawan-E41181232 <arlopaz.uye121299@gmail.com>
2020-06-19 20:49:38 +0700

    coba

---
+++ Backup/app/src/main/java/com/kelompok4/jtipkl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kelompok4.jtipkl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
